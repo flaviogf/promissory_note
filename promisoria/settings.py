c3ca6e82635c000e1a3b7bb2f1a55dca5a73ef08
flaviogf <flavio.fernandes6@gmail.com>
2019-02-13 18:52:59 -0200

    projeto e template commom

--- promisoria/settings.py
+++ promisoria/settings.py
@@ -29,9 +29,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin', 'django.contrib.auth',
-    'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'contatos'
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'widget_tweaks',
+    'commom',
+    'contatos',
 ]
 
 MIDDLEWARE = [
