c2028fdfecf4f4c5b5df4378028682cb80d4e1e7
flaviogf <flavio.fernandes6@gmail.com>
2019-03-10 21:29:05 -0300

    totais contas dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -21,10 +21,19 @@ class DashboardView(View):
             conta__contato__usuario=request.user
         ).count()
         total_atividades = total_atividades_contato + total_atividades_conta
-        totais = [
+        totais_atividades = [
             {"label": "Contatos", "total": total_contato},
             {"label": "Atividades", "total": total_atividades},
             {"label": "Contas", "total": total_contas},
+        ]
+        contas = list(Conta.objects.all())
+        contas_a_receber = sum([c.valor for c in contas if not c.recebida])
+        contas_recebidas = sum([c.valor for c in contas if c.recebida])
+        total_contas = contas_a_receber + contas_recebidas
+        totais_contas = [
+            {"label": "Contas a receber", "total": contas_a_receber},
+            {"label": "Contas recebidas", "total": contas_recebidas},
+            {"label": "Total", "total": total_contas},
         ]
         historicos_contatos = HistoricoContato.objects.filter(
             contato__usuario=request.user
@@ -36,7 +45,8 @@ class DashboardView(View):
             request,
             "dashboard/index.html",
             {
-                "totais": totais,
+                "totais_contas": totais_contas,
+                "totais_atividades": totais_atividades,
                 "historicos_contatos": historicos_contatos,
                 "historicos_contas": historicos_contas,
             },
