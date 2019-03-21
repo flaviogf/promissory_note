1ed3fb93a435bdb69c18bde611dcff7f190881ed
flavio <flavio.fernandes6@gmail.com>
2019-03-21 13:04:38 -0300

    promisoria serializer

--- core/serializers.py
+++ core/serializers.py
@@ -7,13 +7,6 @@ from enderecos.models import Endereco
 
 
 class PromisoriaSerializer(ModelSerializer):
-    contato = HyperlinkedRelatedField(
-        queryset=Contato.objects.all(), view_name='contato-detail')
-    endereco = HyperlinkedRelatedField(
-        queryset=Endereco.objects.all(), view_name='endereco-detail')
-    contas = HyperlinkedRelatedField(
-        queryset=Conta.objects.all(), view_name='conta-detail', many=True)
-
     class Meta:
         model = Promisoria
         fields = '__all__'
