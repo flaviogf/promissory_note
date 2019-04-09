8d100532f29bf36cd167e02605b77c9baf8cd6b7
flaviogf <flavio.fernandes6@gmail.com>
2019-04-08 22:49:16 -0300

    beneficiario

--- promissoria/domain/entities.py
+++ promissoria/domain/entities.py
@@ -3,16 +3,11 @@ from common.domain.events import EventPublisher
 from promissoria.domain.events import EmitenteCadastrado
 
 
-class Emitente(Entity):
-    def __init__(self, nome: 'str', cpf: 'str', endereco: 'str'):
+class Pessoa(Entity):
+    def __init__(self, nome: 'str', cpf: 'str'):
         super().__init__()
         self._nome = nome
         self._cpf = cpf
-        self._endereco = endereco
-
-        EventPublisher.instancia().publish(EmitenteCadastrado(nome=nome,
-                                                              cpf=cpf,
-                                                              endereco=endereco))
 
     @property
     def nome(self) -> 'str':
@@ -22,6 +17,33 @@ class Emitente(Entity):
     def cpf(self) -> 'str':
         return self._cpf
 
+
+class Emitente(Pessoa):
+    def __init__(self, nome: 'str', cpf: 'str', endereco: 'str'):
+        super().__init__(nome, cpf)
+        self._endereco = endereco
+
     @property
     def endereco(self) -> 'str':
         return self._endereco
+
+    @staticmethod
+    def novo(nome: 'str', cpf: 'str', endereco: 'str') -> 'Emitente':
+        emitente = Emitente(nome=nome,
+                            cpf=cpf,
+                            endereco=endereco)
+
+        EventPublisher.instancia().publish(EmitenteCadastrado(nome=nome,
+                                                              cpf=cpf,
+                                                              endereco=endereco))
+
+        return emitente
+
+
+class Beneficiario(Pessoa):
+    @staticmethod
+    def novo(nome: 'str', cpf: 'str') -> 'Beneficiario':
+        beneficiario = Beneficiario(nome=nome,
+                                    cpf=cpf)
+
+        return beneficiario
--- promissoria/domain/tests/test_entities.py
+++ promissoria/domain/tests/test_entities.py
@@ -2,7 +2,7 @@ import unittest
 import uuid
 from unittest.mock import patch, Mock
 
-from promissoria.domain.entities import Emitente
+from promissoria.domain.entities import Emitente, Beneficiario
 
 
 class EmitenteTests(unittest.TestCase):
@@ -12,13 +12,28 @@ class EmitenteTests(unittest.TestCase):
         self.nome = 'tony'
         self.cpf = '12345678900'
         self.endereco = 'Rua x'
-        self.sut = Emitente(nome=self.nome,
-                            cpf=self.cpf,
-                            endereco=self.endereco)
+        self.sut = Emitente.novo(nome=self.nome,
+                                 cpf=self.cpf,
+                                 endereco=self.endereco)
 
-    def test_init(self) -> 'None':
+    def test_novo(self) -> 'None':
         self.assertIsInstance(self.sut.id, uuid.UUID)
         self.assertEqual(self.nome, self.sut.nome)
         self.assertEqual(self.cpf, self.sut.cpf)
         self.assertEqual(self.endereco, self.sut.endereco)
         self.publisher.instancia().publish.assert_called_once()
+
+
+class BeneficiarioTests(unittest.TestCase):
+    @patch('promissoria.domain.entities.EventPublisher')
+    def setUp(self, publisher: 'Mock') -> 'None':
+        self.publiser = publisher
+        self.nome = 'steve'
+        self.cpf = '12312312311'
+        self.sut = Beneficiario.novo(nome=self.nome,
+                                     cpf=self.cpf)
+
+    def test_novo(self) -> 'None':
+        self.assertIsInstance(self.sut.id, uuid.UUID)
+        self.assertEqual(self.nome, self.sut.nome)
+        self.assertEqual(self.cpf, self.sut.cpf)
