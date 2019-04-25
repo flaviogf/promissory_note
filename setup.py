a8bacd2fe56e76a3249c5849921954931e5da9e6
flavio garcia fernandes <flavio@Mac-mini-de-flavio.local>
2019-04-25 08:59:51 -0300

    configuration setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='promissory_note',
+    version='0.0.1',
+    description='Simple promissory note issuance',
+    long_description='Simple promissory note issuance',
+    url='https://github.com/flaviogf/promissory_note',
+    author='Flavio Fernandes',
+    author_email='flavio.fernandes6@gmail.com',
+    packages=('promissory_note',),
+    install_requires=('pyflunt==1.0.0',)
+)
