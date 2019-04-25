d5f9a3d3625a298a07a379d7b2105c125478abe3
flaviogf <flavio.fernandes6@gmail.com>
2019-04-25 13:04:11 -0300

    feat_emitter
    - [x] Emitter
      - [x] name
      - [x] cpf
      - [x] address
      - [x] email

--- promissory_note/entities.py
+++ promissory_note/entities.py
@@ -1,4 +1,5 @@
 from pyflunt.notifications import Notifiable
+from pyflunt.validations import Contract
 
 
 class Beneficiary(Notifiable):
@@ -9,3 +10,19 @@ class Beneficiary(Notifiable):
         self._email = email
 
         self.add_notifications(name, cpf, email)
+
+
+class Emitter(Notifiable):
+    def __init__(self, name, cpf, address, email):
+        super().__init__()
+        self._name = name
+        self._cpf = cpf
+        self._address = address
+        self._email = email
+
+        contract = Contract().requires().has_min_len(value=address,
+                                                     minimum=6,
+                                                     field='address',
+                                                     message='invalid address')
+
+        self.add_notifications(contract, name, cpf, email)
--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,6 +1,6 @@
 import unittest
 
-from promissory_note.entities import Beneficiary
+from promissory_note.entities import Beneficiary, Emitter
 from promissory_note.value_objects import Name, Email, Cpf
 
 
@@ -48,3 +48,85 @@ class BeneficiaryTests(unittest.TestCase):
                                   email=email)
 
         self.assertFalse(beneficiary.is_valid)
+
+
+class EmitterTests(unittest.TestCase):
+    def test_should_is_valid_true_when_all_properties_are_valid(self):
+        name = Name('Steve')
+        cpf = Cpf('11111111199')
+        address = 'New York'
+        email = Email('captain@marvel.com.br')
+
+        emitter = Emitter(name=name,
+                          cpf=cpf,
+                          address=address,
+                          email=email)
+
+        self.assertTrue(emitter.is_valid)
+
+    def test_should_is_valid_false_when_name_is_invalid(self):
+        name = Name('')
+        cpf = Cpf('11111111199')
+        address = 'New York'
+        email = Email('captain@marvel.com.br')
+
+        emitter = Emitter(name=name,
+                          cpf=cpf,
+                          address=address,
+                          email=email)
+
+        self.assertFalse(emitter.is_valid)
+
+    def test_should_is_valid_false_when_cpf_is_invalid(self):
+        name = Name('Steve')
+        cpf = Cpf('1111111119')
+        address = 'New York'
+        email = Email('captain@marvel.com.br')
+
+        emitter = Emitter(name=name,
+                          cpf=cpf,
+                          address=address,
+                          email=email)
+
+        self.assertFalse(emitter.is_valid)
+
+    def test_should_is_valid_false_when_address_is_invalid(self):
+        name = Name('Steve')
+        cpf = Cpf('11111111199')
+        address = 'New'
+        email = Email('captain@marvel.com.br')
+
+        emitter = Emitter(name=name,
+                          cpf=cpf,
+                          address=address,
+                          email=email)
+
+        self.assertFalse(emitter.is_valid)
+
+    def test_should_notifications_must_contain_invalid_address_message_when_address_is_invalid(self):
+        name = Name('Steve')
+        cpf = Cpf('11111111199')
+        address = 'New'
+        email = Email('captain@marvel.com.br')
+
+        emitter = Emitter(name=name,
+                          cpf=cpf,
+                          address=address,
+                          email=email)
+
+        invalid_address_notification = emitter.notifications[0]
+
+        self.assertEqual('invalid address', invalid_address_notification.message)
+
+    def test_should_is_valid_false_when_email_is_invalid(self):
+        name = Name('Steve')
+        cpf = Cpf('11111111199')
+        address = 'New York'
+        email = Email('invalid email')
+
+        emitter = Emitter(name=name,
+                          cpf=cpf,
+                          address=address,
+                          email=email)
+
+        self.assertFalse(emitter.is_valid)
