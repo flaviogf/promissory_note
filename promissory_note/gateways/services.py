89f5e79a51046844a9f17b45c4524526441c428f
flaviogf <flavio.fernandes6@gmail.com>
2019-04-27 21:46:14 -0300

    implementation PillowImageGenerationService

--- promissory_note/gateways/services.py
+++ promissory_note/gateways/services.py
@@ -1,7 +1,10 @@
+import locale
 from os import path
 from os.path import dirname
 
 from PIL import Image, ImageDraw, ImageFont
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 ROOT_DIR = dirname(dirname(dirname(__file__)))
 
@@ -15,17 +18,53 @@ TEXT_FONT = ImageFont.truetype(OPEN_SANS, size=16)
 
 TEXT_COLOR = 'rgb(0,0,0)'
 
-NUMBER_POSITION = 200, 32
+NUMBER_POSITION = 200, 34
 
-DUE_DATE_DAY_POSITION = 348, 32
+DUE_DATE_DAY_POSITION = 348, 34
 
-DUE_DATE_MONTH_POSITION = 410, 32
+DUE_DATE_MONTH_POSITION = 410, 34
+
+DUE_DATE_YEAR_POSITION = 550, 34
+
+LONG_DUE_DATE_POSITION = 205, 62
+
+VALUE_POSITION = 650, 34
+
+FULL_VALUE_POSITION = 370, 148
+
+BENEFICIARY_NAME_POSITION = 180, 118
+
+BENEFICIARY_NAME_CPF_POSITION = 615, 118
+
+CITY_AND_STATE_PAYMENT_POSITION = 425, 210
+
+EMITTER_NAME_POSITION = 225, 238
+
+ISSUANCE_DATE_DAY_POSITION = 650, 238
+
+ISSUANCE_DATE_MONTH_POSITION = 695, 238
+
+ISSUANCE_DATE_YEAR_POSITION = 735, 238
+
+EMITTER_CPF_POSITION = 225, 264
+
+EMITTER_ADDRESS_POSITION = 525, 264
+
+
+def write(fn):
+    def wrapper(self, *args, **kwargs):
+        fn(self, *args, **kwargs)
+        self._write()
+
+    return wrapper
 
 
 class PillowImageGenerationService:
     def __init__(self):
         self._image = None
         self._draw = None
+        self._position = None
+        self._text = None
         self._filename = None
         self._promissory_note_issued = None
 
@@ -38,14 +77,19 @@ class PillowImageGenerationService:
         self._open_image()
 
         self._write_number()
-        self._write_due_date()
+        self._write_due_date_day()
+        self._write_due_date_month()
+        self._write_due_date_year()
+        self._write_long_due_date()
         self._write_value()
+        self._write_full_value()
         self._write_beneficiary_name()
         self._write_beneficiary_cpf()
-        self._write_currency()
-        self._write_city_payment()
+        self._write_city_and_state_payment()
         self._write_emitter_name()
-        self._write_issuance_date()
+        self._write_issuance_date_day()
+        self._write_issuance_date_month()
+        self._write_issuance_date_year()
         self._write_emitter_cpf()
         self._write_emitter_address()
 
@@ -57,8 +101,85 @@ class PillowImageGenerationService:
         self._image = Image.open(PROMISSORY_NOTE_IMAGE)
         self._draw = ImageDraw.Draw(self._image)
 
+    @write
     def _write_number(self):
-        self._draw.text(NUMBER_POSITION, str(self._promissory_note_issued.number), fill=TEXT_COLOR, font=TEXT_FONT)
+        self._position = NUMBER_POSITION
+        self._text = str(self._promissory_note_issued.number)
+
+    @write
+    def _write_due_date_day(self):
+        self._position = DUE_DATE_DAY_POSITION
+        self._text = self._promissory_note_issued.due_date.strftime('%d')
+
+    @write
+    def _write_due_date_month(self):
+        self._position = DUE_DATE_MONTH_POSITION
+        self._text = self._promissory_note_issued.due_date.strftime('%B')
+
+    @write
+    def _write_due_date_year(self):
+        self._position = DUE_DATE_YEAR_POSITION
+        self._text = self._promissory_note_issued.due_date.strftime('%Y')
+
+    @write
+    def _write_long_due_date(self):
+        self._position = LONG_DUE_DATE_POSITION
+        self._text = self._promissory_note_issued.due_date.strftime('%d de %B de %Y')
+
+    @write
+    def _write_value(self):
+        self._position = VALUE_POSITION
+        self._text = f'{self._promissory_note_issued.value:.2f}'
+
+    @write
+    def _write_full_value(self):
+        self._position = FULL_VALUE_POSITION
+        self._text = f'R$ {self._promissory_note_issued.value:.2f}'
+
+    @write
+    def _write_beneficiary_name(self):
+        self._position = BENEFICIARY_NAME_POSITION
+        self._text = self._promissory_note_issued.beneficiary_name
+
+    @write
+    def _write_beneficiary_cpf(self):
+        self._position = BENEFICIARY_NAME_CPF_POSITION
+        self._text = self._promissory_note_issued.beneficiary_cpf
+
+    @write
+    def _write_city_and_state_payment(self):
+        self._position = CITY_AND_STATE_PAYMENT_POSITION
+        self._text = f'{self._promissory_note_issued.city_payment} - {self._promissory_note_issued.state_payment}'
+
+    @write
+    def _write_emitter_name(self):
+        self._position = EMITTER_NAME_POSITION
+        self._text = self._promissory_note_issued.emitter_name
+
+    @write
+    def _write_issuance_date_day(self):
+        self._position = ISSUANCE_DATE_DAY_POSITION
+        self._text = self._promissory_note_issued.issuance_date.strftime('%d')
+
+    @write
+    def _write_issuance_date_month(self):
+        self._position = ISSUANCE_DATE_MONTH_POSITION
+        self._text = self._promissory_note_issued.issuance_date.strftime('%m')
+
+    @write
+    def _write_issuance_date_year(self):
+        self._position = ISSUANCE_DATE_YEAR_POSITION
+        self._text = self._promissory_note_issued.issuance_date.strftime('%y')
+
+    @write
+    def _write_emitter_cpf(self):
+        self._position = EMITTER_CPF_POSITION
+        self._text = self._promissory_note_issued.emitter_cpf
+
+    @write
+    def _write_emitter_address(self):
+        self._position = EMITTER_ADDRESS_POSITION
+        self._text = self._promissory_note_issued.emitter_address
 
     def _create_filename(self):
         basename = f'{self._promissory_note_issued.number}_promissory_note.jpg'
@@ -67,45 +188,5 @@ class PillowImageGenerationService:
     def _save_image(self):
         self._image.save(self._filename)
 
-    def _write_due_date(self):
-        day = self._promissory_note_issued.due_date.strftime('%d')
-        month = self._promissory_note_issued.due_date.strftime('%B')
-        self._draw.text(DUE_DATE_DAY_POSITION, day, fill=TEXT_COLOR, font=TEXT_FONT)
-        self._draw.text(DUE_DATE_MONTH_POSITION, month, fill=TEXT_COLOR, font=TEXT_FONT)
-        pass
-
-    def _write_value(self):
-        # TODO: write value
-        pass
-
-    def _write_beneficiary_name(self):
-        # TODO: write beneficiary name
-        pass
-
-    def _write_beneficiary_cpf(self):
-        # TODO: write beneficiary cpf
-        pass
-
-    def _write_currency(self):
-        # TODO: write currency
-        pass
-
-    def _write_city_payment(self):
-        # TODO: write city payment
-        pass
-
-    def _write_emitter_name(self):
-        # TODO: write emitter name
-        pass
-
-    def _write_issuance_date(self):
-        # TODO: write issuance date
-        pass
-
-    def _write_emitter_cpf(self):
-        # TODO: write emitter cpf
-        pass
-
-    def _write_emitter_address(self):
-        # TODO: write emitter address
-        pass
+    def _write(self):
+        self._draw.text(self._position, self._text, fill=TEXT_COLOR, font=TEXT_FONT)
