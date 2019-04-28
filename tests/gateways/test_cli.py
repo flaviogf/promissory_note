eac20ddd997f76cb2bbdf17726b07d98a1cb325b
flaviogf <flavio.fernandes6@gmail.com>
2019-04-28 14:06:46 -0300

    implementation CLI

--- tests/gateways/test_cli.py
+++ tests/gateways/test_cli.py
@@ -1,36 +1,100 @@
 import unittest
+from datetime import date, datetime
 from os import chdir, listdir, path, remove
 from unittest.mock import patch
 
-from promissory_note.gateways.cli import promissory_note
+from promissory_note.gateways.cli import Cli, print_green, print_blue
 from promissory_note.gateways.services import CONTENT_DIR, PROMISSORY_NOTE_IMAGE, OPEN_SANS
+
+
+class PrintGreenTests(unittest.TestCase):
+    @patch('builtins.print')
+    def test_should_print_text_when_print_green_is_called_with_text(self, print):
+        print_green('Tony', begin='\n')
+
+        print.assert_called_with('\033[32m\nTony\033[0m')
+
+
+class PrintBlueTests(unittest.TestCase):
+    @patch('builtins.print')
+    def test_should_print_text_when_print_green_is_called_with_text(self, print):
+        print_blue('Tony', begin='\n')
+
+        print.assert_called_with('\033[34m\nTony\033[0m')
 
 
 class CliTests(unittest.TestCase):
     def setUp(self):
+        self._cli = Cli()
+        self._execute_issued_promissory_note()
         self._remove_created_images()
 
     def tearDown(self):
         self._remove_created_images()
 
-    @patch('builtins.print')
+    @patch('promissory_note.gateways.cli.print_green')
     def test_should_cli_print_promissory_note_issued_success(self, print):
-        promissory_note(number=1,
-                        due_date='12/12/2020',
-                        value=100.99,
-                        beneficiary_name='Steve',
-                        beneficiary_cpf='11111111199',
-                        beneficiary_email='captain@marvel.com',
-                        currency='real',
-                        city_payment='New York',
-                        state_payment='New York',
-                        emitter_name='Tony Stark',
-                        emitter_cpf='11111111188',
-                        emitter_address='New York',
-                        emitter_email='iron_man@marvel.com',
-                        issuance_date='12/12/2020')
+        self._execute_issued_promissory_note()
 
-        print.assert_called_with('Emission request sent')
+        print.assert_called_with('Emission request sent !!!', begin='\n\n')
+
+    def test_should_cli_contains_number(self):
+        self.assertEqual(1, self._cli.number)
+
+    def test_should_cli_contains_due_date(self):
+        self.assertEqual(date(year=2020, month=12, day=12), self._cli.due_date)
+
+    def test_should_cli_contains_value(self):
+        self.assertEqual(100.99, self._cli.value)
+
+    def test_should_cli_contains_beneficiary_name(self):
+        self.assertEqual('Steve', self._cli.beneficiary_name)
+
+    def test_should_cli_contains_beneficiary_cpf(self):
+        self.assertEqual('11111111199', self._cli.beneficiary_cpf)
+
+    def test_should_cli_contains_beneficiary_email(self):
+        self.assertEqual('captain@marvel.com', self._cli.beneficiary_email)
+
+    def test_should_cli_contains_currency(self):
+        self.assertEqual('real', self._cli.currency)
+
+    def test_should_cli_contains_city_payment(self):
+        self.assertEqual('New York', self._cli.city_payment)
+
+    def test_should_cli_contains_state_payment(self):
+        self.assertEqual('New York', self._cli.state_payment)
+
+    def test_should_cli_contains_emitter_name(self):
+        self.assertEqual('Tony Stark', self._cli.emitter_name)
+
+    def test_should_cli_contains_emitter_cpf(self):
+        self.assertEqual('11111111188', self._cli.emitter_cpf)
+
+    def test_should_cli_contains_emitter_address(self):
+        self.assertEqual('New York', self._cli.emitter_address)
+
+    def test_should_cli_contains_emitter_email(self):
+        self.assertEqual('iron_man@marvel.com', self._cli.emitter_email)
+
+    def test_should_cli_contains_issuance_date(self):
+        self.assertEqual(date(year=2020, month=12, day=12), self._cli.issuance_date)
+
+    def _execute_issued_promissory_note(self):
+        self._cli.issued_promissory_note(number=1,
+                                         due_date=datetime(day=12, month=12, year=2020),
+                                         value=100.99,
+                                         beneficiary_name='Steve',
+                                         beneficiary_cpf='11111111199',
+                                         beneficiary_email='captain@marvel.com',
+                                         currency='real',
+                                         city_payment='New York',
+                                         state_payment='New York',
+                                         emitter_name='Tony Stark',
+                                         emitter_cpf='11111111188',
+                                         emitter_address='New York',
+                                         emitter_email='iron_man@marvel.com',
+                                         issuance_date=datetime(day=12, month=12, year=2020))
 
     def _remove_created_images(self):
         chdir(CONTENT_DIR)
